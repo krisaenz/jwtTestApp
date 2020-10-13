d3e53c8b10e331d155833b874e0386c3c39831fe
Kristian Saenz <ksaenz@brikit.com>
2020-10-13 10:08:12 -0500

    setting up api with jwt and devise

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+Rails api ulilizing devise and jwt
---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+
+  private
+
+  # Give us a nice json response
+  def respond_with(resource, _opts = {})
+    render json: {
+    status: status,
+    data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+    }
+  end
+  
+  def respond_to_on_destroy
+    head :ok
+  end
+
+end
